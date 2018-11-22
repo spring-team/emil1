35a7bfdbd36d0d7ec67ce96a32d04b9a6b76a77d
Rod Johnson <johnsonroda@gmail.com>
2018-11-22 11:17:54 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/neo/Emil1Application.java
@@ -0,0 +1,12 @@
+package com.neo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Emil1Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Emil1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/neo/Emil1Controller.java
@@ -0,0 +1,20 @@
+package com.neo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class Emil1Controller {
+
+	@GetMapping("/")
+	public String root() {
+		return "Hello Malmo Sweden! I'm served by " + getClass().getName();
+	}
+
+	@GetMapping("hello/{name}")
+	public String person(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+
+}
